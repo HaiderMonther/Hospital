0e89931dcfa28737f765b9d9714f009b7a684721
HaiderMonther <alardyhydr567890@gmail.com>
2023-09-11 19:49:12 +0300

    not don

--- models/db_controller.js
+++ models/db_controller.js
@@ -21,24 +21,24 @@ con.connect(function(err){
 });
 
 module.exports.signup = function(username, email, password, status, callback){
-    con.query('SELECT email FROM users WHERE email = `${email}`'),
+    con.query('SELECT email FROM users WHERE email = "'+email+'" ',
     function(err, result){
         if (result[0]==undefined){
-            var query= "INSERT INTO `users`(`username`, `email`, `password`, `email_status`) VALUES(`${username}, ${email}, ${password}, ${status}`)"
+            var query= "insert into `user`(`username`, `email`, `password`, `email_status`) VALUES('"+username+"', '"+email+"', '"+password+"','"+status+"')"
         console.log(query);    
         } else{
             console.log("error")
         }
-    }
+    })
 }
 
 module.exports.verify=function(username, email, token, callback){
-    var query = "insert into `verify`(`username`, `email`, `token`) values (`${username}, ${email}, {token}`) "
+    var query = "insert into `verify`(`username`, `email`, `token`) values ('"+username+"', '"+email+"','"+token+"') "
     con.query(query, callback)
 }
 
 
-module.exports.verify=function(email, callback){
-    var query = "select *from verify where email = `${email}` "
+module.exports.getuserid=function(email, callback){
+    var query = "select *from verify where email = '"+email+"' "
     con.query(query, callback)
 }
